b1a5b4108e6c9b44cd7998f69c50aea18cb0a144
Gordan Graberec <g.graberec@hotmail.com>
2016-04-14 11:25:18 +0200

    grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+/**
+ * Created by gordan on 14.04.16..
+ */
+
+module.exports = function(grunt){
+
+    const appSrcFiles = ['app/*.js', 'app/**/*.js'];
+    const serverSrcFiles = ['server/*.js', 'server/**/*.js'];
+    const srcFiles = appSrcFiles.concat(serverSrcFiles);
+
+    grunt.initConfig({
+
+        watch:{
+            hint:{
+                files: srcFiles,
+                tasks:['jshint']
+            },
+
+            transpile:{
+                files: appSrcFiles,
+                tasks: ['browserify']
+            }
+        },
+
+        jshint:{
+            all: srcFiles,
+            options:{
+                esnext: true
+            }
+        },
+
+        browserify:{
+            transpile:{
+                files:{
+                    'public/bundle.js': appSrcFiles
+                },
+                options:{
+                    transform: [
+                        ['babelify', {
+                            presets: 'es2015'
+                        }]
+                    ]
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-browserify');
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['browserify']);
+
+}
