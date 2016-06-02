23da7fab81b0c7beac2f0917afb1372c17840071
Gordan Graberec <g.graberec@hotmail.com>
2016-06-02 09:05:19 +0200

    arrow validacija

--- bridge/validation-schemes/student.js
+++ bridge/validation-schemes/student.js
@@ -5,13 +5,13 @@
 module.exports = {
     firstName: [
         [(value) => /^[a-zšđčćžA-ŽŠĐČĆŽ'-]*$/.test(value), 'First name must consist of alphanumerics.'],
-        [function(value) { return value && value.length === 0; }, 'First name is required.']
+        [(value) => value && value.length === 0, 'First name is required.']
     ],
     middleName: [
-        [function(value) { return /^[a-zšđčćžA-ŽŠĐČĆŽ'-]*$/.test(value) }, 'Middle name must consist of alphanumerics.']
+        [(value) => /^[a-zšđčćžA-ŽŠĐČĆŽ'-]*$/.test(value), 'Middle name must consist of alphanumerics.']
     ],
     lastName: [
-        [function(value) { return /^[a-zšđčćžA-ŽŠĐČĆŽ-]*$/.test(value) }, 'Last name must consist of alphanumerics.'],
-        [function(value) { return value && value.length === 0; }, 'Last name is required.']
+        [(value) => /^[a-zšđčćžA-ŽŠĐČĆŽ-]*$/.test(value), 'Last name must consist of alphanumerics.'],
+        [(value) => value && value.length === 0, 'Last name is required.']
     ]
 };
